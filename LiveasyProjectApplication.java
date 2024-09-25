22fbf2336f552ed22c47b559974b0d2ebdd8729a
Er. Amit Kumar <121847237+amit7974@users.noreply.github.com>
2024-09-25 15:57:07 +0530

    Add files via upload

---
+++ LiveasyProjectApplication.java
@@ -0,0 +1,13 @@
+package coder_amit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveasyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveasyProjectApplication.class, args);
+	}
+
+}
